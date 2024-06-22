bfcb98459bfaa70fbaef9eb2b72875825a1345b4
Lewis Odero <lewisodero27@gmail.com>
2024-06-23 02:05:55 +0300

    fix: fix the slider

--- js/index.js
+++ js/index.js
@@ -99,6 +99,18 @@ $(document).ready(function () {
 		lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
 	});
 
+//  <!-- Generate 128 bubbles with random styles -->
+	 for (var i = 0; i < 128; i++) {
+			const bubble = document.createElement("div");
+			bubble.className = "bubble";
+			bubble.style.setProperty("--size", `${2 + Math.random() * 4}rem`);
+			bubble.style.setProperty("--distance", `${6 + Math.random() * 4}rem`);
+			bubble.style.setProperty("--position", `${-5 + Math.random() * 110}%`);
+			bubble.style.setProperty("--time", `${2 + Math.random() * 2}s`);
+			bubble.style.setProperty("--delay", `${-1 * (2 + Math.random() * 2)}s`);
+			document.querySelector(".bubbles").appendChild(bubble);
+		}
+
 	// Initial call to set the slider CSS
 	setSliderCss();
 	findCurrentTabSelector();
